7bee4bdb08181268fbd66c648fb78762f0201a14
Ikenna Richard <oguejioforrichard@gmail.com>
2022-12-20 00:54:20 -1100

    Update page files

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+//notification count
+const notificationCount = document.querySelector('.notifications')
+
+//mark all as read
+const markAll = document.querySelector('.mark_all');
+
+const red_dot = [...document.querySelectorAll('.red_dot')];
+
+const notificationDisplay = document.querySelectorAll('.top_three .figure');
+
+let content = [...document.querySelectorAll('.content_link')];
+
+//read notification
+notificationDisplay.forEach((item, i) => {
+    
+    item.addEventListener('click', (e)=> {
+        e.preventDefault();
+        item.classList.add('none')
+        content[i].style.color = 'var(--dark-grayish-blue)';
+
+        red_dot[i].style.display = 'none';
+    } )
+})
+
+
+//mark all as read
+markAll.addEventListener ('click', (e) => {
+    notificationDisplay.forEach((item, i) => {
+        e.preventDefault();
+        item.classList.add('none')
+        content[i].style.color = 'var(--dark-grayish-blue)';
+
+        red_dot[i].style.display = 'none';e.preventDefault();
+        item.classList.add('none')
+        content[i].style.color = 'var(--dark-grayish-blue)';
+
+        red_dot[i].style.display = 'none';
+
+        notificationCount.innerText = 0;
+    })
+})
